8fd7b980f52e97320184b30bf99c980f22850425
Jamie <54751722+MaroonAngel@users.noreply.github.com>
2020-09-17 12:24:20 -0600

    Added cooked egg

--- src/main/java/net/maroonangel/cultivation/item/Items.java
+++ src/main/java/net/maroonangel/cultivation/item/Items.java
@@ -2,6 +2,7 @@ package net.maroonangel.cultivation.item;
 
 import net.minecraft.block.Blocks;
 import net.minecraft.item.AliasedBlockItem;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.maroonangel.cultivation.Cultivation;
 import net.minecraft.item.ItemGroup;
@@ -13,8 +14,9 @@ public class Items {
     public static final Item IRON_DUST = new Item((new Item.Settings().group(Cultivation.GROUP)));
     public static final Item MORTAR_AND_PESTLE = new Item((new Item.Settings().group(Cultivation.GROUP).maxCount(1)));
     public static final Item PLANT_FIBERS = new Item((new Item.Settings().group(Cultivation.GROUP)));
-    //public static final Item SIEVE = new Item((new Item.Settings().group(Cultivation.GROUP).recipeRemainder(Items.SIEVE).maxCount(1)));
     public static final Item PEBBLE = new Item((new Item.Settings().group(Cultivation.GROUP)));
+
+    public static final Item COOKED_EGG = new Item((new Item.Settings().group(Cultivation.GROUP).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.5F).build())));
 
 
     public static void register() {
@@ -22,7 +24,7 @@ public class Items {
         Registry.register(Registry.ITEM, new Identifier("cultivation", "iron_dust"), IRON_DUST);
         Registry.register(Registry.ITEM, new Identifier("cultivation", "mortar_and_pestle"), MORTAR_AND_PESTLE);
         Registry.register(Registry.ITEM, new Identifier("cultivation", "plant_fibers"), PLANT_FIBERS);
-        //Registry.register(Registry.ITEM, new Identifier("cultivation", "sieve"), SIEVE);
         Registry.register(Registry.ITEM, new Identifier("cultivation", "pebble"), PEBBLE);
+        Registry.register(Registry.ITEM, new Identifier("cultivation", "cooked_egg"), COOKED_EGG);
     }
 }
